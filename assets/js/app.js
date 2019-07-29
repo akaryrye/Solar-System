903b2340142458011fbdf09ce4e103b8dddc061c
akaryrye <ralldrin@gmail.com>
2019-07-28 23:55:02 -0700

    progress .. if only a tiny bit,

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,20 @@
 let url = "";
 let query = "";
 
-$('#test-button').on('click', function () {
 
-    query = $("input").val();
-    console.log(query);
-
-    url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}`;
+// queries wikipedia
+$('.star').on('click', function (e) {
+    e.preventDefault
+    query = $(this).attr("data");
+    url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=1&titles=${query}`;
 
     $.ajax({
         url: "https://cors-anywhere.herokuapp.com/" + url,
-        method: "POST"
+        method: "GET"
     }).then(function (response) {
         console.log(response);
         $("#test").empty();
-        for (let i = 0; i < 10; i++) {
-            let extract = response.query.search[i].snippet;
-            $("#test").append(`<h3>${i}: ${extract}</h3>`)
-        }
+        let key = Object.keys(response.query.pages)[0]
+        $("#test").html(response.query.pages[key].extract)
     });
 })
-
-
-//api.php?action=query&prop=extracts&exchars=175
