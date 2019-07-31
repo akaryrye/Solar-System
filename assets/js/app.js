b2c6874af1b6df664154960c48ad91c17c68c225
akaryrye <ralldrin@gmail.com>
2019-07-30 21:16:49 -0700

    new branch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 let url = "";
 let query = "";
 
+// Wikipedia URL: https://en.wikipedia.org/wiki/ + (data:planet name)
 
 // queries wikipedia
 $('.planet').on('click', function (e) {
@@ -18,3 +19,31 @@ $('.planet').on('click', function (e) {
         $("#test").html(response.query.pages[key].extract)
     });
 })
+
+let nasaKEY = "ejYfEsz7YUb0d2SpoVde1mSoOdWgiYBAL1jKWbwO"
+let nasaURL = "https://api.nasa.gov/planetary/apod?api_key="
+
+$.ajax({
+    url: nasaURL + nasaKey,
+    success: function (result) {
+        if ("copyright" in result) {
+            $("#copyright").text("Image Credits: " + result.copyright);
+        }
+        else {
+            $("#copyright").text("Image Credits: " + "Public Domain");
+        }
+
+        if (result.media_type == "video") {
+            $("#apod_img_id").css("display", "none");
+            $("#apod_vid_id").attr("src", result.url);
+        }
+        else {
+            $("#apod_vid_id").css("display", "none");
+            $("#apod_img_id").attr("src", result.url);
+        }
+        $("#reqObject").text(url);
+        $("#returnObject").text(JSON.stringify(result, null, 4));
+        $("#apod_explaination").text(result.explanation);
+        $("#apod_title").text(result.title);
+    }
+});
